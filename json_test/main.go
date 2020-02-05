c8c78c49baa2724c779f3716ce7e47207104696d
Daniel Vedå <danieved@stud.ntnu.no>
2020-02-05 19:35:53 +0100

    adds encodeJson and decodeJson

--- json_test/main.go
+++ json_test/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"encoding/json"
 	"strings"
+	"reflect"
 )
 
 type Message1 struct {
@@ -11,7 +12,7 @@ type Message1 struct {
 	Number int
 }
 
-type Message2 struct {
+type Msg struct {
 	Age int
 	LastName string
 }
@@ -19,7 +20,7 @@ type Message2 struct {
 func printJSON(ch <- chan string){
 
 	var m1 Message1
-	var m2 Message2
+	var m2 Msg
 	
 	for{
 		select{
@@ -54,19 +55,66 @@ func printJSON(ch <- chan string){
 	}
 }
 
-// Should have a function to add correct prefix to json object.
-// func addPrefix
+// Encode to JSON and add type prefix
+// param: msg interface{} : The message to send as an empty interface. Empty interface allows
+//                          generic data types to be used
+func encodeJson(msg interface{}){
+
+
+	fmt.Printf("\n\nStarted encodejson\n")
+
+	//Encode message as JSON 
+	msgJson, err := json.Marshal(msg)
+
+	if(err != nil){
+		fmt.Println("Error: encodeJson failed at Marshal")
+	}
+
+	//Check which data type the message is
+	msgType := string(reflect.TypeOf(msg).Name())
+	fmt.Printf("EncodeJson: Type: %s, length: %d\n\n", msgType, len(msgType))
+
+	msgJson = []byte(msgType+string(msgJson))
+	fmt.Printf("Encoded Json with prefix:\n%s\n\n", string(msgJson))
+	//Add prefix based on datatype
+	
+}
+
+// Decode JSON to correct type based on prefix
+// param msg interface - Prefixed json message to decode
+// Might need a list of all types available to decode to
+func decodeJson(msg interface{}){
+	fmt.Printf("\n\nStarted decodeJSON\n\n")
+
+	//Assert that msg is a byte array
+	if msg1, ok := msg.([]byte); ok{
+		//Read prefix to check which type
+
+		msgType := strings.SplitN(string(msg1), "{", 1)
+		fmt.Printf("Decoded JSON:\nmsg:%s\n", string(msg1))
+		fmt.Printf("Decoded JSON:\nmsgType:%s\n", msgType)
+	}
+	// if strings.HasPrefix(string(msg), "Message1") {	
+	//Unmarshal to that type
+	//return correct datatype
+}
 
 func main() {
 	fmt.Println("JSON conversion test")
 	jsonChan := make(chan string) //channel for sending JSON objects as string
 
+	
+	
 
 	//Create two messages of different types
 	message1 := Message1{Name: "Daniel", Number: 42}
-	message2 := Message2{Age: 100, LastName: "Vedå"}
+	message2 := Msg{Age: 100, LastName: "Vedå"}
 
 	go printJSON(jsonChan) //start print function
+
+	encodeJson(message1)
+	encodeJson(message2)
+	
 
 
 	//send both messages as strings
@@ -78,11 +126,14 @@ func main() {
 	json2 = []byte("Message2"+string(json2))
 	fmt.Println(string(json1))
 	fmt.Println(string(json2))
+
+
+	decodeJson(json1)
 	
 	jsonChan <- string(json1)
 	jsonChan <- string(json2)
 
-	fmt.Scanln()
+	fmt.Scanln()	 
 }
 
 //  LocalWords:  JSON
