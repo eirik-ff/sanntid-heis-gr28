7ed49deb5b664a60d99fd75c839c51a40d6b04bd
Daniel Vedå <danieved@stud.ntnu.no>
2020-02-19 18:42:14 +0100

    adds custom network module based on the bcast driver. Adds readme for it

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sanntid-heis-gr28
 Heisprosjekt i Sanntid
+
+## Network Module
+[main.go](main.go) provides an example of how to use the network module.
--- main.go
+++ main.go
@@ -1,75 +1,65 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
-	"os"
 	"time"
-
-	"./network/bcast"
-	"./network/localip"
+	"./network"
 )
 
-// We define some custom struct to send over the network.
-// Note that all members we want to transmit must be public. Any private members
-//  will be received as zero-values.
-type HelloMsg struct {
-	Message string
-	Iter    int
+type Msg1 struct {
+	Text string
+	I int
 }
 
-type Order struct {
-	Floor     int
-	Direction int
+type Msg2 struct {
+	Text string
 }
 
 func main() {
-	// Our id can be anything. Here we pass it on the command line, using
-	//  `go run main.go -id=our_id`
-	var id string
-	flag.StringVar(&id, "id", "", "id of this peer")
-	flag.Parse()
+	receiveChan1 := make(chan Msg1) //Chan for receiving msg1
+	receiveChan2 := make(chan Msg2) //chan for receiving msg2
+	txChan := make(chan interface{})
+	
+	msg1 := Msg1{Text: "This is a struct of Msg1", I: 0}
+	msg2 := Msg2{Text: "This is a struct of Msg2"} 
 
-	// ... or alternatively, we can use the local IP address.
-	// (But since we can run multiple programs on the same PC, we also append the
-	//  process ID)
-	if id == "" {
-		localIP, err := localip.LocalIP()
-		if err != nil {
-			fmt.Println(err)
-			localIP = "DISCONNECTED"
-		}
-		id = fmt.Sprintf("peer-%s-%d", localIP, os.Getpid())
-	}
+	port := 20020
+	go network.Transmitter(port, txChan)
+	go network.Receiver(port, receiveChan1, receiveChan2)
 
-	generalTx := make(chan string)
-	generalRx := make(chan string)
-	go bcast.Transmitter(54321, "28", generalTx)
-	go bcast.Receiver(54321, "28", generalRx)
 
-	// The example message. We just send one of these every second.
-	go func() {
-		msg := HelloMsg{Message: "Hello from " + id, Iter: 0}
-		for {
-			msg.Iter++
-
-			// will be its own function
-			jsonbyte, _ := json.Marshal(msg)
-			send := string(jsonbyte)
-
-			generalTx <- string(send)
-			time.Sleep(1 * time.Second)
+	//Rx func
+	go func(){
+		for{
+			select{
+				case msg := <- receiveChan1:
+				fmt.Println("Received a Msg1")
+				fmt.Println("Content: ", msg)
+				break;
+				
+				case msg := <- receiveChan2:
+				fmt.Println("Received a Msg2")
+				fmt.Println("Content: ", msg)
+				break;
+			}
 		}
 	}()
+	
+	//Tx func
+	go func(){
+		for{
 
-	fmt.Println("Started")
-	for {
-		select {
-		case a := <-generalRx:
-			var recv HelloMsg
-			json.Unmarshal([]byte(a), &recv)
-			fmt.Printf("Received: %s %d\n", recv.Message, recv.Iter)
+			txChan <- msg1 //send message 1 to receiver
+			time.Sleep(500 * time.Millisecond)
+			
+			txChan <- msg2 //send message 2 to receiver
+			msg1.I = msg1.I + 1
+			
+			time.Sleep(500 * time.Millisecond)
 		}
-	}
+	}()
+	
+	fmt.Scanln()	 
 }
+
+//  LocalWords:  JSON
